b35a1540adaa4b725f4b41e0e283c0930c973965
Lahiru Bandara <lahirub@i360.lk>
2019-09-22 19:38:37 +0530

    transaction removed from the service

--- dev/src/main/java/com/loits/aml/repo/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.loits.aml.repo;
-
-import com.loits.aml.domain.Transaction;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TransactionRepository extends PagingAndSortingRepository<Transaction, Integer>, QuerydslPredicateExecutor<Transaction> {
-}
