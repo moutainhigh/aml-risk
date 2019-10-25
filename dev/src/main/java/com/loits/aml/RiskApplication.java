a8b8fb00e53d660b6c9ff4bbe097759c68c15205
Minoli_d <minolid@i360.lk>
2019-10-25 10:24:42 +0530

    test-deploy

--- dev/src/main/java/com/loits/aml/RiskApplication.java
+++ dev/src/main/java/com/loits/aml/RiskApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
@@ -23,4 +25,10 @@ public class RiskApplication {
         return new SpelAwareProxyProjectionFactory();
     }
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer props = new PropertySourcesPlaceholderConfigurer();
+        props.setLocations(new ClassPathResource("service-ref.properties"));
+        return props;
+    }
 }
