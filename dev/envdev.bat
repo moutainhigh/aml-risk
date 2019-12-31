e740d0f161ae2e64568f61f39cab76705f8c58ec
Minoli_d <minolid@i360.lk>
2019-12-31 16:35:15 +0530

    dev working code

---
+++ dev/envdev.bat
@@ -0,0 +1,19 @@
+@echo off
+set "DB_HOST=130.61.46.215"
+set "DB_PORT=3306
+set "DB_USER=amluser"
+set "DB_PASS=aml2841@fusionX"
+set "CONFIG_SERVER_URL=http://132.145.233.0:8080"
+set "CONF_SERVER_USR=admin"
+set "CONF_SERVER_PASS=trypassword"
+set "CONF_ENV_NAME=dev"
+@echo on
+echo "DB_HOST: "%DB_HOST%
+echo "DB_PORT: "%DB_PORT%
+echo "DB_USER: "%DB_USER%
+echo "DB_PASS: "%DB_PASS%
+echo "CONFIG_SERVER_URL: "%CONF_URL_PORT%
+echo "CONF_SERVER_USR: "%CONF_SERVER_USR%
+echo "CONF_SERVER_PASS: "%CONF_SERVER_PASS%
+echo "CONF_ENV_NAME: "%CONF_ENV_NAME%
+@echo off
